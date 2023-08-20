fbaeec28694a6809bc8100541071bc355461d55c
David Munyiri <davidmumyiri2019@gmail.com>
2023-08-20 15:23:36 +0300

    completed making all the API's

--- Backend/Contoller/productController.js
+++ Backend/Contoller/productController.js
@@ -1,2 +1,107 @@
 const mssql=require('mssql')
-const {v4}=require('uuid')
+const {v4}=require('uuid') 
+const { sqlConfig } = require('../Config/config')
+
+const addProduct=async(req,res)=>{
+    try {
+        
+    const productId = v4()
+    const {productName,productDescription,productClassification,productCategory,productCost,productImg,earlyCost} = req.body
+    const pool = await mssql.connect(sqlConfig)
+    
+        const result = await pool.request()
+        .input('productId', mssql.VarChar, productId)
+        .input('productName', mssql.VarChar, productName)
+        .input('productDescription', mssql.VarChar, productDescription)
+        .input('productClassification', mssql.VarChar, productClassification)
+        .input('productCategory',productCategory)
+        .input('productCost',productCost)
+        .input('productImg',productImg)
+        .input('earlyCost',earlyCost)
+        .execute('addProductPROC')
+
+    
+         if(result.rowsAffected[0]==1){  
+         return res.status(200).json({
+             message: "Product added Succesfully",
+         })}
+         else{
+             return res.status(400).json({message: "Product adding failed"})
+         }
+    } catch (error) {
+        return res.status(400).json({Error:error.message})
+    }
+}
+
+const updateProduct=async(req,res)=>{
+    try {
+        const productId=req.params.productId
+        const {productName,productDescription,productClassification,productCategory,productCost,productImg,earlyCost} = req.body
+        const pool = await mssql.connect(sqlConfig)
+    
+        const result = await pool.request()
+        .input('productId', mssql.VarChar, productId)
+        .input('productName', mssql.VarChar, productName)
+        .input('productDescription', mssql.VarChar, productDescription)
+        .input('productClassification', mssql.VarChar, productClassification)
+        .input('productCategory',productCategory)
+        .input('productCost',productCost)
+        .input('productImg',productImg)
+        .input('earlyCost',earlyCost)
+        .execute('updateProductPROC')
+
+    
+         if(result.rowsAffected[0]==1){  
+         return res.status(200).json({
+             message: "Product Update Succesfully",
+         })}
+         else{
+             return res.status(400).json({message: "Product Update failed"})
+         }
+
+        
+    } catch (error) {
+       return res.status(400).json({Error:error.message}) 
+    }
+}
+
+//VIEW ONE PRODUCT
+const viewOneProduct=async(req,res)=>{
+    try { 
+        const productId=req.params.productId
+        const pool=await mssql.connect(sqlConfig)
+        const oneProduct=(await pool.request()
+        .input('productId',mssql.VarChar, productId)
+        .execute('viewOneProductProc')).recordset[0]
+
+        if(oneProduct){
+            return res.status(200).json({message:'Here is the product',oneProduct})
+        } else{
+            return res.status(400).json({message:'Failed to fetch The Product'})
+        }
+        
+    } catch (error) {
+        return res.status(401).json({Error:error.message})
+    }
+}
+
+//VIEW ALL PRODUCTS 
+const viewAllproducts=async(req,res)=>{
+    try {
+        const pool=await mssql.connect(sqlConfig)
+        const allProducts=(await pool.request()
+        .execute('viewAllproductsProc')).recordsets
+        if(allProducts){
+            return res.status(200).json({message:"Here are all products",allProducts})
+        }
+        else{
+            return res.status(400).json({message:'Failed To Fetch products'})
+        }
+    } catch (error) {
+        return res.status(401).json({Error:error.message})
+    }
+}
+
+module.exports={
+    addProduct,updateProduct,viewOneProduct,viewAllproducts
+}
